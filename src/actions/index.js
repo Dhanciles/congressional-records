009c281237401b7179eaafac57ae30f1d23ecefb
Derek Hanciles <drhanciles@icloud.com>
2019-01-05 14:20:18 -0700

    Action creators for isLoading, hasErrored, fetchDataSuccess, searchSubject have been implemented

---
+++ src/actions/index.js
@@ -0,0 +1,19 @@
+export const isLoading = (check) => ({
+  type: 'IS_LOADING', 
+  isLoading: check
+})
+
+export const hasErrored = (check) => ({
+  type: 'HAS_ERRORED', 
+  hasErrored: check
+})
+
+export const fetchDataSucces = (data) => ({
+  type: 'FETCH_DATA_SUCCESS', 
+  data
+})
+
+export const seachSubject = (text) => ({
+  type: 'SEARCH_SUBJECT', 
+  text
+})
