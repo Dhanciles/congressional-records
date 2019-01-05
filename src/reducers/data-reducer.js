ce862c8bbba79954aa815c4b8b412a36ed60efcb
Derek Hanciles <drhanciles@icloud.com>
2019-01-05 16:22:57 -0700

    Updated fetchDataSuccess action to now have a query parameter and updated dataReducer to dynamically update the query

--- src/reducers/data-reducer.js
+++ src/reducers/data-reducer.js
@@ -1,7 +1,7 @@
 export const dataReducer = (state = {}, action) => {
   switch(action.type) {
     case 'FETCH_DATA_SUCCESS': 
-      return {...state, [key]: [action.data]}
+      return {...state, [action.query]: [action.data]}
     default: 
       return state
   }
