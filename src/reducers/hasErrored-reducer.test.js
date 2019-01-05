16f5b216fa8d52a489ec0cfc14291a0aa24a9bf5
Derek Hanciles <drhanciles@icloud.com>
2019-01-05 16:59:11 -0700

    Test checking if hasErroredReducer is updating state if we receive an error has been impelemented and is passing

--- src/reducers/hasErrored-reducer.test.js
+++ src/reducers/hasErrored-reducer.test.js
@@ -8,4 +8,16 @@ describe('hasErroredReducer', () => {
 
     expect(result).toEqual(expected)
   })
+  it('should update state if there is an error', () => {
+    const action = {
+      type: 'HAS_ERRORED', 
+      hasErrored: true
+    }
+
+    const expected  = true
+
+    const result = hasErroredReducer(false, action)
+
+    expect(result).toEqual(expected)
+  })
 })
