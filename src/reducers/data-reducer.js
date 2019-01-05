ae38fd6931f59a293af8a2fde5167a99eae670c9
Derek Hanciles <drhanciles@icloud.com>
2019-01-05 16:04:37 -0700

    Tweaked dataReducer to now not directly mutate state

--- src/reducers/data-reducer.js
+++ src/reducers/data-reducer.js
@@ -1,9 +1,7 @@
 export const dataReducer = (state = {}, action) => {
   switch(action.type) {
     case 'FETCH_DATA_SUCCESS': 
-      return {
-        [key]: action.data
-      }
+      return {...state, [key]: [action.data]}
     default: 
       return state
   }
