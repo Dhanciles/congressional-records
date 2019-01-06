9473718eaa0db45f4e1b6b26e0d36fd38f086b5c
Derek Hanciles <drhanciles@icloud.com>
2019-01-05 22:26:06 -0700

    Implemented a thunk that will fetch data that we need based on the query passed in

---
+++ src/thunks/fetchData.js
@@ -0,0 +1,21 @@
+import { isLoading, hasErrored, fetchDataSuccess } from '../actions/index.js'; 
+import { cleanBill } from '../helper/helper.js'; 
+
+export const fetchData = (url, query) => {
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true))
+      const response = await fetch(url)
+      if(!response.ok) {
+        throw Error(response.statusText)
+      }
+      dispatch(isLoading(false))
+      const data = await response.json()
+      const dataToStore = await cleanBill(data)
+      dispatch(fetchDataSuccess(dataToStore, query))
+    } catch(error) {
+      dispatch(hasErrored(false))
+    }
+  }
+
+}
