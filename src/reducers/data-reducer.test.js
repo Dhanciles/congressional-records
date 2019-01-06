44fb5cab54fe93a60fb9aadb9aa4c7e1dd7fd912
Derek Hanciles <drhanciles@icloud.com>
2019-01-05 17:37:19 -0700

    Test checking if dataReducer is updating state has been impelemented and is passing

--- src/reducers/data-reducer.test.js
+++ src/reducers/data-reducer.test.js
@@ -8,4 +8,19 @@ describe('dataReducer', () => {
 
     expect(result).toEqual(expected)
   })
+
+  it('should update state with our query as a key with a value of the data requested when a request is made', () => {
+    const mockData = [1, 2, 3, 4, 5]
+    const action = {
+      type: 'FETCH_DATA_SUCCESS', 
+      query: 'immigration bills', 
+      data: mockData
+    }
+
+    const expected = {[action.query]: action.data}
+
+    const result = dataReducer({}, action)
+
+    expect(result).toEqual(expected)
+  })
 })
