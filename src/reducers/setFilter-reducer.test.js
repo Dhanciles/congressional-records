9e0e8ad1c6fdfb1f41f10a0b2cfe2e1871d94606
Derek Hanciles <drhanciles@icloud.com>
2019-01-05 17:22:57 -0700

    Test checking if setFilterReduer is updating state when a filter has been set has been impelemented and is passing

--- src/reducers/setFilter-reducer.test.js
+++ src/reducers/setFilter-reducer.test.js
@@ -8,4 +8,16 @@ describe('setFilterReducer', () => {
 
     expect(result).toEqual(expected)
   })
+  it('should update state if a new filter has been set', () => {
+    const action = {
+      type: 'SET_FILTER', 
+      filter: 'Recent Votes'
+    }
+
+    const expected = 'Recent Votes'
+
+    const result = setFilterReducer('', action)
+
+    expect(result).toEqual(expected)
+  })
 })
