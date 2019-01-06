2226af187514baa72e1fe3c253fa7e82bcf052d4
Derek Hanciles <drhanciles@icloud.com>
2019-01-05 17:06:45 -0700

    Test checking if isLoadingReducer is updating state has been implemented and is passing

--- src/reducers/isLoading-reducer.test.js
+++ src/reducers/isLoading-reducer.test.js
@@ -8,4 +8,16 @@ describe('isLoadingReducer', () => {
 
     expect(result).toEqual(expected)
   })
+  it('should update state if our request is loading', () => {
+    const action = {
+      type: 'IS_LOADING', 
+      isLoading: true
+    }
+
+    const expected = true
+
+    const result = isLoadingReducer(false, action)
+
+    expect(result).toEqual(expected)
+  })
 })
