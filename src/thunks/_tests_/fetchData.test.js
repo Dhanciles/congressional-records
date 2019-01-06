cbfcb77942a93eab14426e831f6800cc8ea091ef
Derek Hanciles <drhanciles@icloud.com>
2019-01-05 23:12:51 -0700

    Test checking if fetchData is calling cleanBill with the correct params has been implemented and is passing

--- src/thunks/_tests_/fetchData.test.js
+++ src/thunks/_tests_/fetchData.test.js
@@ -1,15 +1,19 @@
 import { fetchData } from '../fetchData'; 
 import { isLoading, hasErrored, fetDatatSuccess } from '../../actions/index.js'; 
+import { cleanBill } from '../../helper/helper.js'; 
+import { immigrationBillData } from '../../../mockBillData.js'; 
 
 describe('fetchData', () => {
   let mockUrl
   let mockQuery 
   let mockDispatch
+  let mockData
 
   beforeEach(() => {
     mockUrl = 'www.getdata.web'
     mockQuery = 'rare data'
     mockDispatch = jest.fn()
+    mockData = immigrationBillData
   })
 
   it('should call dispatch with the isLoading action', async () => {
@@ -40,5 +44,18 @@ describe('fetchData', () => {
     await thunk(mockDispatch)
 
     expect(mockDispatch).toHaveBeenCalledWith(isLoading(false))
+  })
+  it('should call cleanBill with the correct params', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: true, 
+      json: () =>  Promise.resolve(mockData)
+    }))
+
+    const thunk = fetchData(mockUrl, mockQuery)
+
+    await thunk(mockDispatch)
+
+    expect(cleanBill).toHaveBeenCalledW
+
   })
 })
