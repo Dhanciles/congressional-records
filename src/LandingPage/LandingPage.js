48692010b060756b36c0fccf8b7bd07c4f44895d
Derek Hanciles <drhanciles@icloud.com>
2019-01-07 13:13:23 -0700

    Implemented structure for LandingPage component

---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react'; 
+import '../styles/LandingPage.scss'; 
+
+export const LandingPage = () => {
+  return (
+    <main>
+      <article className="title-container">
+        <h1>Congressional Records</h1>
+      </article>
+      <div className="logo-container">
+        <img src='./icons/congress.svg' alt='capitol building'/>
+      </div>
+      <article className="text-container">
+        <h3>Harness Information That WIll Keep You Informed and Inspire You To Get Involved</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+      </article>
+    </main>
+  )
+}
