1c965d322ee15a2844580001f2d47deba83bf444
Derek Hanciles <drhanciles@icloud.com>
2019-01-06 21:46:38 -0700

    Implemented structure of SearchInput component

---
+++ src/SearchInput/SearchInput.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export class SearchInput extends Component {
+  constructor() {
+    super()
+    this.state = {
+      subject = ''
+    }
+  }
+  
+  handleChange = (e) => {
+    const { value } = e.target
+    this.setState({
+      subject: value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({
+      subject: ''
+    })
+  }
+
+  render() {
+    const searchIcon = '../src/icons/search.svg'
+    return (
+      <form onSubmit={this.handleSubmit()}>
+        <input onChange={this.handleChange()}type="text" name="search" value={this.state.subject} placeholder="Search"/>
+        <img className="search-icon" src={searchIcon}/>
+      </form>
+    )
+  }
+}
