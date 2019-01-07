837977c3c717373769fa39d9946b7b218fe1456d
Derek Hanciles <drhanciles@icloud.com>
2019-01-07 08:46:08 -0700

    fixed naming issue with hasErroredReducer in root reducer file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,11 @@ import { combineReducers } from 'redux'
 import { dataReducer } from './data-reducer'
 import { setFilterReducer } from './setFilter-reducer'
 import { isLoadingReducer } from './isLoading-reducer'
-import { hasErroredreducer } from './hasErrored-reducer'
+import { hasErroredReducer } from './hasErrored-reducer'
 
 export const rootReducer = combineReducers({
   loading: isLoadingReducer, 
-  error: hasErroredreducer, 
+  error: hasErroredReducer, 
   filterSelection: setFilterReducer, 
   data: dataReducer
 })
