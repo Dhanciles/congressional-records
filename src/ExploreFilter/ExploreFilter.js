58475caf6f602bf9d70e690369e3a03b458fc6d9
Derek Hanciles <drhanciles@icloud.com>
2019-01-06 23:09:25 -0700

    Imstalled uuid and implemented key using uuid in ExploreFilter component

--- src/ExploreFilter/ExploreFilter.js
+++ src/ExploreFilter/ExploreFilter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { uuid } from 'uuid'
 // import '../src/styles/ExploreFilter.scss'
 
 export class ExploreFilter extends Component {
@@ -11,10 +12,10 @@ export class ExploreFilter extends Component {
 
   render() {
     const exploreIcon = this.state.selected ? '..src/icons/up-arrow.svg': '../src/icons/down-arrow.svg'
-    const baseExploreItems = ['Recent Votes', 'Recent Bills', 'Upcoming Bills']
+    const basicExploreItems = ['Recent Votes', 'Recent Bills', 'Upcoming Bills']
     const popularSubjects = ['Immigration', 'Climate', 'Criminal Justice', 'Police Brutality', 'Higher Education', 'Russia', 'Terrorism', 'Crimes Against Women', 'Fires', 'Mental Health', 'Oil and Gas'] 
-    const baseList = baseExploreItems.map(item => (<li>{item}</li>))
-    const popularList = popularSubjects.map(subject => (<li>{subject}</li>))
+    const basicList = basicExploreItems.map(item => (<li className="basic-item" key={uuid()}>{item}</li>))
+    const popularList = popularSubjects.map(subject => (<li className="popular-subject" key={uuid()}>{subject}</li>))
     const defaultRender = (
       <div className="explore-menu">
         <h3>Explore</h3>
@@ -27,7 +28,7 @@ export class ExploreFilter extends Component {
           <img className="explore-arrow-icon" src={exploreIcon} alt="Down or up arrows used to represent if the Explore menu has been selected."/>
         <div className="selected-menu-container">
           <article className="base-list">
-            <ul>{baseList}</ul>
+            <ul>{basicList}</ul>
           </article>
           <article className="popular-subjects-list">
             <ul>{popularList}</ul>
