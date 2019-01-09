658fa11083bf045f9e44bd9138c04d76d511a23d
Derek Hanciles <drhanciles@icloud.com>
2019-01-08 22:49:58 -0700

    Impelemented snapshot test in LandingPage component and it is passing

---
+++ src/components/LandingPage/LandingPage.test.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+import { shallow } from 'enzyme'; 
+import { LandingPage } from './LandingPage'; 
+
+describe('LandingPage', () => {
+  it('should match the snapshot', () => {
+    const wrapper = shallow(<LandingPage />)
+
+    expect(wrapper).toMatchSnapshot()
+  })
+})
